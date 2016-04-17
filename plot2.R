f9ecc18bfec64385091efc401d3df0bb44e84131
João Silva <joaoa.comp@gmail.com>
2016-04-17 14:28:45 -0300

    Script to create plot3

--- plot2.R
+++ plot2.R
@@ -9,8 +9,7 @@ plot2 <- function(data=NULL) {
      png("plot2.png", width=480, height=480)
      
      plot(data$Time, data$Global_active_power,
-          type="l",
-          xlab="",
+          type="l", xlab="",
           ylab="Global Active Power (kilowatts)")
      
      dev.off()
---
+++ plot3.R
@@ -0,0 +1,24 @@
+source("load_data.R")
+
+plot3 <- function(data=NULL) {
+     if(is.null(data))
+          data <- load_data()
+     
+     png("plot3.png", width=480, height=480)
+     
+     #makes the plot
+     plot(data$Time, data$Sub_metering_1, type="l", col="black",
+          xlab="", ylab="Energy sub metering")
+     #add legend
+     legend("topright", col=c("black", "red", "blue"),
+            c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+            lty=1)
+     #add lines
+     lines(data$Time, data$Sub_metering_2, col="red")
+     lines(data$Time, data$Sub_metering_3, col="blue")
+     
+     
+     dev.off()
+}
+
+
