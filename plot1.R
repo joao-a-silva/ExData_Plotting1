87a7c996d902d271b7e4f38a9a6f9cdb84586daa
João Silva <joaoa.comp@gmail.com>
2016-04-17 10:39:46 -0300

    Update plot1

--- plot1.R
+++ plot1.R
@@ -4,8 +4,8 @@ plot1 <- function(data=NULL) {
      if(is.null(data))
           data <- load_data()
      
-     png("plot1.png", width=400, height=400)
-     hist(as.numeric(data$Global_active_power), xlab ="Global Active Power - Kilowatts", 
+     png("plot1.png", width=480, height=480)
+     hist(as.numeric(data$Global_active_power), xlab ="Global Active Power (Kilowatts)", 
           main = "Global Active Power", col="Red")
      dev.off()
 }
